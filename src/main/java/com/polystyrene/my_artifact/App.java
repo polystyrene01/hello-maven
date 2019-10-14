ebf2fa2015ff15523867f1d376fae61c0b339bcb
polystyrene <polystyrene01@gmail.com>
2019-10-13 22:55:41 -0700

    commons-logging

--- src/main/java/com/polystyrene/my_artifact/App.java
+++ src/main/java/com/polystyrene/my_artifact/App.java
@@ -1,13 +1,14 @@
 package com.polystyrene.my_artifact;
 
-/**
- * Hello world!
- *
- */
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Log log = LogFactory.getLog(App.class);
+        log.info("Hello, world!");
     }
 }
