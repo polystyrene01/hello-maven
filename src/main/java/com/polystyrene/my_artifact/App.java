075db9aa9fbb9e35028b1c96dcd65b7bee169187
polystyrene <polystyrene01@gmail.com>
2019-10-13 21:10:27 -0700

    first maven

---
+++ src/main/java/com/polystyrene/my_artifact/App.java
@@ -0,0 +1,13 @@
+package com.polystyrene.my_artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
